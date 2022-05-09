a668c72b9d1694f82e97dbd13eb263730300aac9
quasarblue <fabrizio.candon@gmail.com>
2022-05-09 17:18:52 +0200

    messages

--- src/main/java/com/github/klimgeran/symbol/discord/treasury/BotConfiguration.java
+++ src/main/java/com/github/klimgeran/symbol/discord/treasury/BotConfiguration.java
@@ -1,11 +1,16 @@
 package com.github.klimgeran.symbol.discord.treasury;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.klimgeran.symbol.discord.treasury.event.EventListener;
+
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
 
 @Configuration
 public class BotConfiguration {
@@ -14,10 +19,17 @@ public class BotConfiguration {
     private String token;
 
     @Bean
-    public GatewayDiscordClient gatewayDiscordClient() {
-        return DiscordClientBuilder.create(token)
-          .build()
-          .login()
-          .block();
+    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
+    	GatewayDiscordClient client = DiscordClientBuilder.create(token).build().login().block();
+    	
+    	for(EventListener<T> listener : eventListeners) {
+            client.on(listener.getEventType())
+              .flatMap(listener::execute)
+              .onErrorResume(listener::handleError)
+              .subscribe();
+        }
+
+        return client;
+    	
     }
 }
---
+++ src/main/java/com/github/klimgeran/symbol/discord/treasury/message/MessageCreateListener.java
@@ -0,0 +1,16 @@
+package com.github.klimgeran.symbol.discord.treasury.message;
+
+
+@Service
+public class MessageCreateListener extends MessageListener implements EventListener<MessageCreateEvent> {
+
+    @Override
+    public Class<MessageCreateEvent> getEventType() {
+        return MessageCreateEvent.class;
+    }
+
+    @Override
+    public Mono<Void> execute(MessageCreateEvent event) {
+        return processCommand(event.getMessage());
+    }
+}
