e0c3c776330302f8de4a7be24e7ff1eb6f967f83
FABRIZIO CANDON <FABRIZIO-CANDON@venezia.intranet.rve>
2022-05-30 12:36:21 +0200

    message

--- src/main/java/com/github/klimgeran/symbol/discord/treasury/event/MessageListener.java
+++ src/main/java/com/github/klimgeran/symbol/discord/treasury/event/MessageListener.java
@@ -17,7 +17,6 @@ import reactor.core.publisher.Mono;
 public abstract class MessageListener {
    	
     public Mono<Void> processCommand(Message eventMessage) {
-    	
     	/* WebSocketClient client = new ReactorNettyWebSocketClient();
     	 client.execute(URI.create("ws://cola-potatochips:3001/ws"), 
     			 session -> session.receive().map(WebSocketMessage::getPayloadAsText).log().then());//.map(WebSocketMessage::getPayloadAsText).log();
@@ -37,7 +36,7 @@ public abstract class MessageListener {
            .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
            .filter(message -> message.getContent().equalsIgnoreCase("!symbol"))
            .flatMap(Message::getChannel)
-           .flatMap(channel -> channel.createMessage("hello world! "+getSymbolAccountInfo()))
+           .flatMap(channel -> channel.createMessage(getSymbolAccountInfo()))
            .then();
     }
     
