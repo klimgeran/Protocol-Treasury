8c49e0986a18c7571bce1b439b6abc7e5789bc58
FABRIZIO CANDON <FABRIZIO-CANDON@venezia.intranet.rve>
2022-05-30 15:45:32 +0200

    mosaic amount

--- src/main/java/com/github/klimgeran/symbol/discord/treasury/event/MessageListener.java
+++ src/main/java/com/github/klimgeran/symbol/discord/treasury/event/MessageListener.java
@@ -1,8 +1,8 @@
 package com.github.klimgeran.symbol.discord.treasury.event;
 
+import java.math.BigInteger;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
-
-import org.springframework.beans.factory.annotation.Value;
 
 import discord4j.core.object.entity.Message;
 import io.nem.symbol.sdk.api.AccountRepository;
@@ -11,6 +11,7 @@ import io.nem.symbol.sdk.infrastructure.vertx.JsonHelperJackson2;
 import io.nem.symbol.sdk.infrastructure.vertx.RepositoryFactoryVertxImpl;
 import io.nem.symbol.sdk.model.account.AccountInfo;
 import io.nem.symbol.sdk.model.account.Address;
+import io.nem.symbol.sdk.model.mosaic.ResolvedMosaic;
 import io.nem.symbol.sdk.model.transaction.JsonHelper;
 import reactor.core.publisher.Mono;
 
@@ -42,6 +43,7 @@ public abstract class MessageListener {
     
     public String getSymbolAccountInfo() {
     	String af = "";
+    	BigInteger amount=BigInteger.ZERO;
     	try (final RepositoryFactory repositoryFactory = new RepositoryFactoryVertxImpl(
                 "https://symbolnode.ninja:3001")) {
                 final AccountRepository accountRepository = repositoryFactory
@@ -53,7 +55,16 @@ public abstract class MessageListener {
                 final Address address = Address.createFromRawAddress(rawAddress);
                 final AccountInfo accountInfo = accountRepository
                     .getAccountInfo(address).toFuture().get();
+                List<ResolvedMosaic> mosaics = accountInfo.getMosaics();
+                
+                for (ResolvedMosaic resolvedMosaic : mosaics) {
+				  if(resolvedMosaic.getIdAsHex().equals("6BED913FA20223F8")) {
+					  amount=resolvedMosaic.getAmount();
+				  }
+				}
+                
                 final JsonHelper helper = new JsonHelperJackson2();
+                
                 af=helper.prettyPrint(accountInfo);
                 System.out.println(af);
                 
@@ -62,6 +73,6 @@ public abstract class MessageListener {
             } catch (InterruptedException ie) {
             	ie.printStackTrace();
             }
-    	return "Symbol Treasure: \n\n Address: NCHEST3QRQS4JZGOO64TH7NFJ2A63YA7TPM5PXI\n";
+    	return "Symbol Treasure: \n\n Address: NCHEST3QRQS4JZGOO64TH7NFJ2A63YA7TPM5PXI\n\nBalance:\n"+amount+" XYM";
     }
 }
